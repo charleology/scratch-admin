31f9035c5f9d222d991fdd1f4ca866dbd6ea7553
charleology <cnbelano@gmail.com>
2022-08-09 00:09:34 +0800

    Edit records.

--- app/src/main/java/com/example/scratch_admin/EditRecord.java
+++ app/src/main/java/com/example/scratch_admin/EditRecord.java
@@ -8,15 +8,15 @@ import android.view.Window;
 import android.widget.Button;
 import android.widget.ImageView;
 
-public class AddRecord extends AppCompatActivity {
+public class EditRecord extends AppCompatActivity {
 
-    ImageView addRecordPicIv, addRecordBackIv;
-    Button saveRecordBtn;
+    ImageView editRecordPicIv, editRecordBackIv;
+    Button saveChangesBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_add_record);
+        setContentView(R.layout.activity_edit_record);
         getSupportActionBar().hide();
 
         //to adopt status bar to the pink header
@@ -24,9 +24,9 @@ public class AddRecord extends AppCompatActivity {
         window.setStatusBarColor(this.getResources().getColor(R.color.pink));
         window.getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
 
-        addRecordBackIv = (ImageView) findViewById(R.id.addRecordBackIv);
-        addRecordPicIv = (ImageView) findViewById(R.id.addRecordPicIv);
-        saveRecordBtn = (Button) findViewById(R.id.saveRecordBtn);
+        editRecordBackIv = (ImageView) findViewById(R.id.editRecordBackIv);
+        editRecordPicIv = (ImageView) findViewById(R.id.editRecordPicIv);
+        saveChangesBtn = (Button) findViewById(R.id.saveChangesBtn);
     }
 
     public void back(View view){
